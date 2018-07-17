9cfaef45eab56ff6ead2dff363d246a67a172d17
Krister Lagergren <kalagergren@gmail.com>
2018-07-16 21:47:01 -0400

    Pushing all the copied-over gas state calculations into remote

---
+++ src/main/java/categories/GasProperty.java
@@ -0,0 +1,14 @@
+package categories;
+
+public enum GasProperty {
+	// @formatter:off
+	TEMPERATURE,
+	PRESSURE,
+	DENSITY,
+	ENTROPY,
+	ENTHALPY,
+	SPEED,
+	MACH_NUMBER,
+	OTHER;
+	// @formatter:on
+}
