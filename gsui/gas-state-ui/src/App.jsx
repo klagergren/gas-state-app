8a848e8b3b1f3e5805abf9aabeda485870fc6d58
Krister <kalagergren@gmail.com>
2021-03-27 16:04:25 -0700

    GSC-5 Now GSUI submits the values in the box to GSA. Horray!

--- gsui/gas-state-ui/src/App.jsx
+++ gsui/gas-state-ui/src/App.jsx
@@ -1,20 +1,25 @@
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import './App.css';
+import { useSelector } from 'react-redux';
 import GasStateInput from './components/GasStateInput';
 import GasStateTable from './components/GasStateTable';
 import Header from './components/Header';
+import { TEMPERATURE_PROP_ID, PRESSURE_PROP_ID } from './constants/constants';
 
 function App() {
+  const temperature = useSelector((state) => state.inputs[TEMPERATURE_PROP_ID]);
+  const pressure = useSelector((state) => state.inputs[PRESSURE_PROP_ID]);
+
   function submitGasStateRequest() {
     axios({
       method: 'post',
       url: 'http://localhost:8080/calculate',
       data: {
         firstInputType: 'TEMPERATURE',
-        firstInputValue: 333,
+        firstInputValue: temperature,
         secondInputType: 'PRESSURE',
-        secondInputValue: 0.00000969,
+        secondInputValue: pressure,
       },
     })
       .then((res) => console.log(res))
@@ -25,11 +30,11 @@ function App() {
     <div className="container">
       <Header />
       <GasStateInput
-        gasPropertyTypeId="temperature"
+        gasPropertyTypeId={TEMPERATURE_PROP_ID}
         gasPropertyTypeDisplay="Temperature (K)"
       />
       <GasStateInput
-        gasPropertyTypeId="pressure"
+        gasPropertyTypeId={PRESSURE_PROP_ID}
         gasPropertyTypeDisplay="Pressure (Pa)"
       />
       <Button
